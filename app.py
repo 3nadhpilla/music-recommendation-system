fba5cd1dd4b8e1394b906390e3cd86d1b5d9edc5
3nadhpilla <trinadhpilla660@gmail.com>
2025-07-24 14:17:14 +0530

    Added Docker support

--- app.py
+++ app.py
@@ -109,8 +109,10 @@ def upload():
         'tracks': results
     })
 
+import os
+
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
+
 
